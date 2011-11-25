70250252be0156181e7b5c70be9156dee0561bbc
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-25 15:56:23 +0000

    git-svn-id: https://plugins.svn.wordpress.org/image-horizontal-reel-scroll-slideshow/trunk@467455 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ help.php
@@ -0,0 +1,15 @@
+<div class="wrap">
+  <h2>Plugin Management</h2>
+  Click "Go to - Image Management" button to add/update/delete the images. <br />
+  Click "Go to - Gallery Setting" button to update gallery setting.
+  <h2>Plugin Configuration</h2>
+  <ul>
+  <li>1. Drag and Drop the Widget</li>
+  <li>2. Add directly in the theme</li>
+  <li>3. short code for pages and posts</li>
+  </ul>
+  <a target="_blank" href='http://www.gopiplus.com/work/2011/05/08/wordpress-plugin-image-horizontal-reel-scroll-slideshow/'>Click here for more help</a>
+  <h2>About Plugin</h2>
+  Plug-in created by <a target="_blank" href='http://www.gopiplus.com/work/'>www.gopiplus.com</a>.<br />
+  Check official website for live demo <a target="_blank" href='http://www.gopiplus.com/work/2011/05/08/wordpress-plugin-image-horizontal-reel-scroll-slideshow/'>Click here</a><br />
+</div>
